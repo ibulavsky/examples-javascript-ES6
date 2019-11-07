89fb578a43654846c5025d54854d69e3b4a50a84
ibulavsky <ibulavsky@gmail.com>
2019-11-07 18:03:49 +0300

    4. template strings, methods

--- src/3_default_parameters.js
+++ src/3_default_parameters.js
@@ -1,20 +1,20 @@
-{
-    const sum = (a, b) => a + b;
-    console.log(sum()); // NaN
-    console.log(sum(1, 2)); // 3
-}
-{
-    const defaultB = 1;
-    const sum = (a = 0, b = defaultB) => a + b;
-    console.log(sum()); // 1
-    console.log(sum(1, 2)); // 3
-}
-{
-    const getDefault = num => num * 3;
-    const sum = (a = 1, b = getDefault(a)) => a + b;
-    console.log(sum()); // 4
-    console.log(sum(1, 2)) // 3
-}
-
+// {
+//     const sum = (a, b) => a + b;
+//     console.log(sum()); // NaN
+//     console.log(sum(1, 2)); // 3
+// }
+// {
+//     const defaultB = 1;
+//     const sum = (a = 0, b = defaultB) => a + b;
+//     console.log(sum()); // 1
+//     console.log(sum(1, 2)); // 3
+// }
+// {
+//     const getDefault = num => num * 3;
+//     const sum = (a = 1, b = getDefault(a)) => a + b;
+//     console.log(sum()); // 4
+//     console.log(sum(1, 2)) // 3
+// }
+//
 
 
---
+++ src/4_template_strings.js
@@ -0,0 +1,28 @@
+// {
+//     const name = 'Igor';
+//     const template = `Hello, ${name}`;
+//     console.log(template);
+// }
+//
+// {
+//     const isVisible = true;
+//     const template = ` ${isVisible ? "I'm Igor" : ''}`;
+//     console.log(template);
+// }
+//
+// // Methods
+// const str = 'Hello';
+// console.log(str.startsWith('He')); // true
+// console.log(str.startsWith('e')); // false
+// console.log(str.endsWith('o')); // true
+//
+// console.log(str.includes('llo')); // true
+//
+// console.log(str.repeat(3)); // 'HelloHelloHello'
+//
+// console.log(str.trim()); // удаляются пробелы слева и справа
+// console.log(str.trimStart());
+// console.log(str.trimEnd());
+//
+// console.log(str.padStart(10, '-')); // -----Hello
+// console.log(str.padEnd(10, '-!')); // Hello-!-!-
