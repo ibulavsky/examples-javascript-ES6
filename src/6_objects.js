d157c7baeb7dd78248f1521949ae2a3ca71be3eb
ibulavsky <ibulavsky@gmail.com>
2019-11-08 16:32:47 +0300

    6. objects methods (is, assign, keys, values)

---
+++ src/6_objects.js
@@ -0,0 +1,38 @@
+// const cityField = 'city';
+// const job = 'Front-end';
+//
+// const person = {
+//     age: '25',
+//     name: 'Igor',
+//     // [cityField + Date.now()]: 'Minsk',
+//     [cityField]: 'Minsk',
+//     job,
+//     'country': 'Belarus',
+//     print: () => 'Person',
+//     toString() {
+//         return Object.keys(this)
+//             .filter(key => key !== 'toString')
+//             .map(key => `${key} - ${this[key]}`)
+//             .join('; ')
+//     }
+// };
+//
+// // console.log(person.toString());
+// // console.log(person.print());
+// // console.log(person);
+//
+// // Methods
+// const first = {a: 1};
+// const second = {b: 2};
+// // проверет на эквивалентность
+// // console.log(Object.is(20, 20));
+// // объединяет в первый объект ключ/значение из последующих
+// // console.log(Object.assign(first, second));
+// const obj = Object.assign({}, first, {c: 3}, second);
+// // console.log(obj);
+// console.log(Object.keys(obj)); // возвращает ключи
+// console.log(Object.values(obj)); // возвращает значения
+
+
+
+
